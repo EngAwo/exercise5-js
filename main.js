c9422e4ab70147242364e893cf99117c8865f92d
EngAwo <ruushaani01@gmail.com>
2026-03-28 09:47:58 +0300

    adding index.html file

---
+++ main.js
@@ -0,0 +1,11 @@
+// arrow function
+const greet = (name) =>{
+     console.log("hellow " + name )
+}
+
+greet("Eng Awo")
+greet("Eng Ali")
+greet("Prof hamdi")
+greet("Eng kaasim")
+greet("Doc Ahmed")
+
